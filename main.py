e6036a59784286bbca2dc880a8f87d138f653b70
mohamedmohy10 <61654046+mohamedmohy10@users.noreply.github.com>
2023-01-14 20:30:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,33 +3,36 @@ from data import data
 import random
 import os
 
-def game(a,score):
-    b = random.choice(data)
-    print(f"COMPARE :\nA: {a['name']}, a {a['description']} from {a['country']}")
-    print(vs)
-    print(f"B: {b['name']}, a {b['description']} from {b['country']}")
-    answer = input("Who has more followers ? Type 'A' or 'B' : ").lower()
-    if (answer == 'a' and a['follower_count'] >= b['follower_count']) or (answer == 'b' and b['follower_count'] >= a['follower_count']):
+def check_answer(account_a,account_b,answer,score): #Checks user response and scores
+    os.system('cls')
+    print(logo)
+    print(f"{account_a['name']} has {account_a['follower_count']}M followers ")
+    print(f"{account_b['name']} has {account_b['follower_count']}M followers\n")
+    if (answer == 'a' and account_a['follower_count'] >= account_b['follower_count']) or (answer == 'b' and account_b['follower_count'] >= account_a['follower_count']):
         score += 1
-        os.system('cls')
-        print(logo)
-        print(f"{a['name']} has {a['follower_count']}M followers ")
-        print(f"{b['name']} has {b['follower_count']}M followers\n")
-        print(f"You are right, current score : {score}")
-        game(b,score)
+        print(f"You are right, current score : {score}\n")
+        print("="*40)
+        game(account_b,score) #continue the game with account_b which is the new value to compare with
     else:
-        os.system('cls')
-        print(logo)
-        print(f"{a['name']} has {a['follower_count']}M followers ")
-        print(f"{b['name']} has {b['follower_count']}M followers\n")
         print(f"Wrong answer, Your final score : {score} ")
         return
 
-score = 0
+def game(account_a = random.choice(data), score = 0):
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+    print("Compare between : \n")
+    print(f"A: {account_a['name']}, a {account_a['description']} from {account_a['country']}")
+    print(vs)
+    print(f"B: {account_b['name']}, a {account_b['description']} from {account_b['country']}")
+    answer = input("Who has more followers ? Type 'A' or 'B' : ").lower()
+
+    check_answer(account_a,account_b,answer,score)
+
+
 os.system('cls')
-a = random.choice(data)
 print(logo)
 
-game(a,score)
+game()
 
 
